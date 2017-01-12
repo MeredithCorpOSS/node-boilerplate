c1175493397b242c1a46f44f08edcbc3bedd82b2
Prathik Shetty <iprathik@gmail.com>
2017-01-12 14:07:45 +0530

    linting fixes

--- config/config.default.js
+++ config/config.default.js
@@ -1,7 +1,11 @@
+/**
+ * Global config
+ * @return {Object} containing all the config
+ */
 function config() {
   return {
-    config_name': 'config_value'
-  }
+    'config_name': 'config_value',
+  };
 };
 
 module.exports = config();
--- config/constant.js
+++ config/constant.js
@@ -1,7 +1,11 @@
+/**
+ * Global constants
+ * @return {Object} containing all the constants
+ */
 function constant() {
   return {
-      'constant_name' : 'constant_value'
-  }
+     'constant_name': 'constant_value',
+  };
 };
 
 module.exports = constant();
